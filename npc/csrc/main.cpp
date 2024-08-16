d32df66279e3776ffa383245a6a34ffabbf8a602
Hjw-Arch <hujinwei121@163.com>
2024-08-16 18:08:45 +0800

    NJU-ProjectN/nemu ics2023 initialized
    NJU-ProjectN/nemu eb63cf3568dbf4e0c3c6ef462e6ec685550fabbc Merge pull request #76 from rijuyuezhu/master

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,43 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <memory>
-#include <verilated.h>
+#include <memory.h>
 #include <assert.h>
+#include "verilated_vcd_c.h"
 #include "Vtop.h"
+#include "verilated.h"
 
-double sc_time_stamp() { return 0; }
-
-int main(int argc, char **argv) {
-    // printf("Hello, ysyx!\n");
-
+int main (int argc, char **argv) {
     if (false && argc && argv) {}
-
-    Verilated::mkdir("logs");
-    
-    const std::unique_ptr<VerilatedContext> contextp{ new VerilatedContext };
-    contextp->debug(0);
-    contextp->randReset(2);
-    contextp->traceEverOn(true);
+    const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+    std::unique_ptr<Vtop> top{new Vtop{contextp.get()}};
     contextp->commandArgs(argc, argv);
+    contextp->traceEverOn(true);
+    VerilatedVcdC* ftp = new VerilatedVcdC;
+    top->trace(ftp, 0);
+    ftp->open("wave.vcd");
 
-    const std::unique_ptr<Vtop> top{new Vtop{contextp.get(), "TOP"}};
     int flag = 0;
+
     while (!contextp->gotFinish() && ++flag < 20) {
-      contextp->timeInc(1);
-      int a = rand() & 1;
-      int b = rand() & 1;
-      top->a = a;
-      top->b = b;
-      top->eval();
-      //printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-      assert(top->f == (a ^ b));
+        int a = rand() & 1;
+        int b = rand() & 1;
+        top->a = a;
+        top->b = b;
+        top->eval();
+        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+        assert(top->f == (a ^ b));
+
+        contextp->timeInc(1);
+
+        ftp->dump(contextp->time()); 
     }
+
     top->final();
 
-#if VM_COVERAGE
-    Verilated::mkdir("logs");
-    contextp->coveragep()->write("logs/coverage.dat");
-#endif
+    ftp->close();
 
-  return 0;
+    return 0;
 }
