8f5b3f9962e7b56cd1d853c72ffd8b6e959d9847
Hjw-Arch <hujinwei121@163.com>
2024-08-16 18:37:49 +0800

    pa0

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-STUID = ysyx_22040000
-STUNAME = 张三
+STUID = ysyx_hjw
+STUNAME = hjw
 
 # DO NOT modify the following code!!!
 
---
+++ npc/csrc/NVBoardTest.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<Vtop.h>
+#include<nvboard.h>
+
+static Vtop dut;
+
+void nvboard_bind_all_pins(Vtop* top);
+
+int main() {
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+
+    while(1) {
+        dut.eval();
+        nvboard_update();
+    }
+}
