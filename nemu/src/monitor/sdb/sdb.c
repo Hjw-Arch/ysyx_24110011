efa5cf292b99cecb6747a43636a8e1af77caf7b9
tracer-ysyx <tracer@ysyx.org>
2024-08-28 23:23:01 +0800

    > compile NEMU ysyx_hjw hjw Linux ysyx 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:23:01 up 2 days,  1:37,  1 user,  load average: 0.03, 0.09, 0.04

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -158,7 +158,8 @@ static int cmd_test_expr(char* args) {
         char *result_str = strtok(buf, " ");
         printf("\n\nresult:%s\n\n", result_str);
         uint32_t result = (uint32_t)atoll(result_str);
-        char *expr_str = result_str + strlen(result_str) + 1;
+        // char *expr_str = result_str + strlen(result_str) + 1;
+        char *expr_str = strtok(NULL, "\n");
         printf("\n\nexpr:%s\n\n", expr_str);
         bool is_success = true;
         uint32_t result_test = expr(expr_str, &is_success);
