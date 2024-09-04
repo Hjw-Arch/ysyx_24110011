86fceccdf37739dd558ffde8124cbc133240b521
tracer-ysyx <tracer@ysyx.org>
2024-09-04 22:41:26 +0800

    > compile NEMU ysyx_hjw hjw Linux ysyx 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:41:26 up  1:17,  1 user,  load average: 0.16, 0.09, 0.05

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -44,10 +44,10 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
     }
     IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
-    if (!diff_wp()) {
-        nemu_state.state = NEMU_STOP;
-        // TODO(); // 回到mainloop
-    }
+    // if (!diff_wp()) {
+    //     nemu_state.state = NEMU_STOP;
+    //     // TODO(); // 回到mainloop
+    // }
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
