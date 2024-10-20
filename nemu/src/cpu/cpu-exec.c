9f8bd04af908ebeed55fc0c4f624726267b7d364
tracer-ysyx <tracer@ysyx.org>
2024-10-20 12:06:47 +0800

    > compile NEMU ysyx_hjw hjw Linux ysyx 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:06:47 up 4 days, 19:05,  1 user,  load average: 0.32, 0.18, 0.11

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -24,7 +24,7 @@
  * This is useful when you use the `si' command.
  * You can modify this value as you want.
  */
-#define MAX_INST_TO_PRINT 10
+#define MAX_INST_TO_PRINT 0
 
 CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
@@ -39,7 +39,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
         log_write("%s\n", _this->logbuf);
     }
 #endif
-    if (0) {
+    if (g_print_step) {
         IFDEF(CONFIG_ITRACE, puts(_this->logbuf));
     }
     IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
