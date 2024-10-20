8c80ee0beeb73be7d327bbb2451d717fb8d73b8c
tracer-ysyx <tracer@ysyx.org>
2024-10-20 14:08:18 +0800

    > compile NEMU ysyx_hjw hjw Linux ysyx 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:08:18 up 4 days, 21:07,  1 user,  load average: 0.15, 0.12, 0.10

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -73,19 +73,19 @@ void iringbuf_display() {
 
         printf("0x%08x: ", iringbuf.addr[index]);
 
-// #ifndef CONFIG_ISA_loongarch32r
-//         char disasm_buf[64];
-//         void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-//         disassemble(disasm_buf, 64, iringbuf.addr[index], (uint8_t *)&iringbuf.inst[index], 4);
-//         printf("%s   ", disasm_buf);
-// #else
-//         p[0] = '\0'; // the upstream llvm does not support loongarch32r
-// #endif
+#ifndef CONFIG_ISA_loongarch32r
+        char disasm_buf[64];
+        void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+        disassemble(disasm_buf, 64, iringbuf.addr[index], (uint8_t *)&iringbuf.inst[index], 4);
+        printf("%s   ", disasm_buf);
+#else
+        p[0] = '\0'; // the upstream llvm does not support loongarch32r
+#endif
 
 
-        printf("%02x ", (iringbuf.inst[index] & 0xff000000) >> 23);
-        printf("%02x ", (iringbuf.inst[index] & 0x00ff0000) >> 15);
-        printf("%02x ", (iringbuf.inst[index] & 0x0000ff00) >> 7);
+        printf("%02x ", (iringbuf.inst[index] & 0xff000000) >> 24);
+        printf("%02x ", (iringbuf.inst[index] & 0x00ff0000) >> 16);
+        printf("%02x ", (iringbuf.inst[index] & 0x0000ff00) >> 8);
         printf("%02x\n",  (uint8_t)iringbuf.inst[index]);
         
         if(index == end_index) break;
