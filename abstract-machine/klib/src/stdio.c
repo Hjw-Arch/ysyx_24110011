0e82fe0d4735c46f6fdc69349711ec425d47d547
tracer-ysyx <tracer@ysyx.org>
2024-11-11 00:18:26 +0800

    > compile NEMU ysyx_hjw hjw Linux ysyx 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:18:26 up  2:52,  1 user,  load average: 0.07, 0.10, 0.07

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,18 +5,31 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-static int num2string(char **out, int num, int base) {
+
+enum {
+    normal = 0,
+    h,
+    hh,
+    l,
+    ll,
+    j,
+    z,
+    t,
+    L,
+};
+
+static int num2string(char *out, int num, int base) {
     int ret = 0;
     // process negative number
     if (num < 0) {
-        *((*out)++) = '-';
+        *(out++) = '-';
         num = -num;
         ret++;
     }
 
     // process zero
     if (num == 0) {
-        *((*out)++) = '0';
+        *(out++) = '0';
         return 1;
     }
 
@@ -30,15 +43,34 @@ static int num2string(char **out, int num, int base) {
 
     while(i-- > 0) {
         if (a[i] >= 10) {
-            *((*out)++) = (char)(a[i] - 10 + 'a');
+            *(out++) = (char)(a[i] - 10 + 'a');
             ret++;
         } else {
-            *((*out)++) = (char)(a[i] + '0');
+            *(out++) = (char)(a[i] + '0');
         }
         ret++;
     }
 
     return ret;
+}
+
+static int simple_pow(int a, int b) {
+    int result = 1;
+    for (int i = 0; i < b; i++) {
+        result *= a;
+    }
+    return result;
+}
+
+static void string2num(int *num, const char *string, int len) {
+    *num = 0;
+    for (int i = len - 1; i >= 0; i--) {
+        if (string[i] > '9' || string[i] < '0') {
+            *num = 0;
+            return;
+        }
+        *num += (string[i] - '0') * simple_pow(10, len - i - 1);
+    }
 }
 
 int printf(const char *fmt, ...) {
@@ -62,10 +94,147 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
             ret++;
         }
         else {
-            switch (fmt[++i]) {
+            i++;
+
+            bool leftAlign = false;
+            bool showSymbol = false;
+            bool replaceFormat = false;
+            bool blankBeforePostiveNum = false;
+            bool paddingCharZero = false;
+            int width = 0;
+            int precision = -1;
+            int length = normal;
+
+            width = length - 1 + length + 1;    // 消除警告
+            width = replaceFormat;
+
+            // flags
+            while(1) {
+                switch (fmt[i]) {
+                    case '-': {
+                        leftAlign = true;
+                        i++;
+                        break;
+                    }
+
+                    case '+': {
+                        showSymbol = true;
+                        blankBeforePostiveNum = false;
+                        i++;
+                        break;
+                    }
+
+                    case '0': {
+                        paddingCharZero = true;
+                        i++;
+                        break;
+                    }
+
+                    case '#': {
+                        replaceFormat = true;
+                        i++;
+                        break;
+                    }
+
+                    case ' ': {
+                        if (showSymbol) {
+                            i++;
+                            break;
+                        }
+
+                        blankBeforePostiveNum = true;
+
+                        break;
+                    }
+                    
+                    default:
+                        goto br;
+                }
+
+                br: break;
+            }
+
+            // width
+            int j;
+            for (j = 0; fmt[i + j] <= '9' || fmt[i + j] >= '0'; j++) ;
+            string2num(&width, &fmt[i], j);
+            i += j;
+
+            // precision
+            if (fmt[i] == '.') {
+                for (j = 0; fmt[i + j] <= '9' || fmt[i + j] >= '0'; j++) ;
+                string2num(&precision, &fmt[i], j);
+                i += j;
+            }
+
+            // length
+            switch (fmt[i])
+            {
+            case 'h': {
+                i++;
+                if (fmt[i] == 'h') {
+                    i++;
+                    length = hh;
+                    break;
+                }
+                length = h;
+                break;
+            }
+
+            case 'l': {
+                i++;
+                if (fmt[i] == 'l') {
+                    i++;
+                    length = ll;
+                    break;
+                }
+                length = l;
+                break;
+            }
+
+            // TODO
+            case 'j':
+            case 'z':
+            case 't':
+            case 'L':
+                length = normal;    // TODO;
+                i++;
+            default:
+                break;
+            }
+
+            switch (fmt[i]) {
                 case 'd': {
                     int num = va_arg(ap, int);
-                    ret += num2string(&out, num, 10);
+                    if (showSymbol && num > 0) *(out++) = '+';
+                    if (blankBeforePostiveNum && num > 0) *(out++) = ' ';
+
+                    char temp_str[20];
+                    int len = num2string(temp_str, num, 10);
+
+                    bool isRequiredWidth = false;
+
+                    if (width > len) {
+                        if (leftAlign || paddingCharZero) {
+                            for (j = 0; j < width - len; j++) {
+                                *(out++) = (paddingCharZero ? '0' : ' ');
+                            }
+                            ret += j;
+                            isRequiredWidth = true;
+                        }
+                    }
+
+                    ret += len;
+                    strncpy(out, temp_str, len);
+                    out += len;
+
+                    if (width > len && !isRequiredWidth) {
+                        for (j = 0; j < width - len; j++) {
+                            *(out++) = ' ';
+                        }
+                        ret += j;
+                    }
+
                     break;
                 }
 
@@ -76,6 +245,10 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
                         ret++;
                     }
                     break;
+                }
+
+                case '%': {
+                    *(out++) = '%';
                 }
 
                 default:
