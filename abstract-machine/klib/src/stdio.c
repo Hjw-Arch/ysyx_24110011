80e2da3dd8263c592716901a54cfd168984bd4ba
tracer-ysyx <tracer@ysyx.org>
2024-11-13 00:59:59 +0800

    > compile NEMU ysyx_hjw hjw Linux ysyx 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:59:59 up 12:49,  1 user,  load average: 0.60, 0.53, 0.35

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -26,55 +26,61 @@ static int width = 0;
 static int precision = -1;  // TODO
 static int length = normal; // TODO
 
-static int handle_int(char *out, char *temp_str, int len) {
-    bool isSatisfiedWidth = false;
-    int j = 0;
-    int ret = 0;
-    if (width > len)
-    {
-        if (!leftAlign || paddingCharZero)
-        {
-            for (j = 0; j < width - len; j++)
-            {
-                *(out++) = (paddingCharZero ? '0' : ' ');
-            }
-            ret += j;
-            isSatisfiedWidth = true;
+static int double2str(char *str, double num, int precision) {
+    int length = 0;  // 记录字符串长度
+
+    // 处理负数
+    if (num < 0) {
+        *str++ = '-';
+        num = -num;  // 将负数转为正数处理
+        length++;
+    }
+
+    // 处理整数部分
+    long long int_part = (long long)num;  // 整数部分
+    double decimal_part = num - int_part;  // 小数部分
+
+    // 将整数部分转换为字符串
+    char temp[50];  // 临时数组用于存储整数部分字符串
+    int i = 0;
+    if (int_part == 0) {
+        temp[i++] = '0';
+    } else {
+        while (int_part > 0) {
+            temp[i++] = (int_part % 10) + '0';
+            int_part /= 10;
         }
     }
 
-    ret += len;
-    strncpy(out, temp_str, len);
-    out += len;
-
-    if (width > len && !isSatisfiedWidth)
-    {
-        for (j = 0; j < width - len; j++)
-        {
-            *(out++) = ' ';
-        }
-        ret += j;
+    // 反转整数部分
+    for (int j = 0; j < i; j++) {
+        str[length++] = temp[i - j - 1];
     }
 
-    return ret;
+    // 处理小数部分
+    if (precision > 0) {
+        str[length++] = '.';  // 添加小数点
+
+        // 将小数部分转换为字符串
+        for (int j = 0; j < precision; j++) {
+            decimal_part *= 10;
+            int decimal_digit = (int)decimal_part;
+            str[length++] = decimal_digit + '0';
+            decimal_part -= decimal_digit;
+        }
+    }
+
+    str[length] = '\0';  // 添加字符串结束符
+
+    return length;  // 返回字符串的长度
 }
 
 static int num2string(char *out, uint64_t num, int base) {
     int ret = 0;
-    // process negative number
-    if (base == 10) {
-        int64_t _num = num;
-        if (_num < 0) {
-            *(out++) = '-';
-            _num = -_num;
-            num = _num;
-            ret++;
-        }
-    }
 
     // process zero
     if (num == 0) {
-        *(out++) = '0';
+        out[ret] = '0';
         return 1;
     }
 
@@ -82,18 +88,226 @@ static int num2string(char *out, uint64_t num, int base) {
     int a[36];
     int i = 0;
     while(num != 0) {
+
         a[i++] = num % base;
         num /= base;
+
     }
 
     while(i-- > 0) {
         if (a[i] >= 10) {
-            *(out++) = (char)(a[i] - 10 + 'a');
-            ret++;
+            out[ret++] = (char)(a[i] - 10 + 'A');
         } else {
-            *(out++) = (char)(a[i] + '0');
+            out[ret++] = (char)(a[i] + '0');
         }
-        ret++;
+    }
+
+    out[ret] = '\0';
+
+    return ret;
+}
+
+static int handle_int(char *out, uint64_t num, bool isSigned) {
+    bool isSatisfiedWidth = false;
+    bool isNegative = false;
+    int diffblank = 0;
+    int ret = 0;
+
+    // 处理有符号数
+
+    // 判断正负，如果是负数会添加负号，占用一位宽度
+    if (isSigned) {
+        if (length == ll) {
+            if ((int64_t)num < 0) {
+                isNegative = true;
+                num = -((int64_t)num);
+                diffblank++;
+            }
+        } else {
+            if ((int)num < 0) {
+                isNegative = true;
+                num = -((int)num);
+                diffblank++;
+            }
+        }
+
+        // 如果是负数并且不是用0补宽度，直接添加负号
+        if (isNegative && paddingCharZero) {
+            out[ret++] = '-';
+        }
+
+        if (isNegative) showSymbol = false, blankBeforePostiveNum = false;
+
+        // 是否需要添加正号或者空格，如果添加，占一位宽度
+        if (length == ll) {
+            if (showSymbol && (int64_t)num > 0) out[ret++] = '+', diffblank++;
+            if (blankBeforePostiveNum && (int64_t)num > 0) out[ret++] = ' ', diffblank++;
+        } else {
+            if (showSymbol && (int)num > 0) out[ret++] = '+', diffblank++;
+            if (blankBeforePostiveNum && (int)num > 0) out[ret++] = ' ', diffblank++;
+        }
+    }
+
+    // 数字转字符串
+    char str[36];
+    int len = num2string(str, num, 10);
+    diffblank += len;
+
+    // 计算需要补几位宽度
+    diffblank = width - diffblank;
+
+    if (diffblank > 0)
+    {   // 如果是右对齐或者使用0补宽度，要在数字前面补
+        if (!leftAlign || paddingCharZero)
+        {
+            for (int j = 0; j < diffblank; j++)
+            {
+                out[ret++] = (paddingCharZero ? '0' : ' ');
+            }
+            isSatisfiedWidth = true;
+        }
+    }
+
+    // 之前没补负号的话现在补一下
+    if (isNegative && !paddingCharZero) out[ret++] = '-';
+
+    strncpy(&out[ret], str, len);
+    ret += len;
+
+    // 如果之前没补宽度，现在补一下
+    if (diffblank > 0 && !isSatisfiedWidth)
+    {
+        for (int j = 0; j < diffblank; j++)
+        {
+            out[ret++] = ' ';
+        }
+    }
+
+    return ret;
+}
+
+static int handle_float(char *out, double num){
+    bool isSatisfiedWidth = false;
+    bool isNegative = false;
+    int diffblank = 0;
+    int ret = 0;
+
+    // 处理有符号数
+
+    // 判断正负，如果是负数会添加负号，占用一位宽度
+    if (num < 0) {
+        isNegative = true;
+        diffblank++;
+        num = -num;
+    }
+
+    // 如果是负数并且不是用0补宽度，直接添加负号
+    if (isNegative && paddingCharZero) {
+        out[ret++] = '-';
+    }
+
+    if (isNegative) showSymbol = false, blankBeforePostiveNum = false;
+
+    // 是否需要添加正号或者空格，如果添加，占一位宽度
+    if (length == ll || length == l) {
+        if (showSymbol && num > 0) out[ret++] = '+', diffblank++;
+        if (blankBeforePostiveNum && num > 0) out[ret++] = ' ', diffblank++;
+    } else {
+        if (showSymbol && num > 0) out[ret++] = '+', diffblank++;
+        if (blankBeforePostiveNum && num > 0) out[ret++] = ' ', diffblank++;
+    }
+
+    // 数字转字符串
+    char str[36];
+    int len = double2str(str, num, precision == -1 ? 6 : precision);
+    diffblank += len;
+
+    // 计算需要补几位宽度
+    diffblank = width - diffblank;
+
+    if (diffblank > 0)
+    {   // 如果是右对齐或者使用0补宽度，要在数字前面补
+        if (!leftAlign || paddingCharZero)
+        {
+            for (int j = 0; j < diffblank; j++)
+            {
+                out[ret++] = (paddingCharZero ? '0' : ' ');
+            }
+            isSatisfiedWidth = true;
+        }
+    }
+
+    // 之前没补负号的话现在补一下
+    if (isNegative && !paddingCharZero) out[ret++] = '-';
+
+    strncpy(&out[ret], str, len);
+    ret += len;
+
+    // 如果之前没补宽度，现在补一下
+    if (diffblank > 0 && !isSatisfiedWidth)
+    {
+        for (int j = 0; j < diffblank; j++)
+        {
+            out[ret++] = ' ';
+        }
+    }
+
+    return ret;
+}
+
+static int handle_x_o(char *out, uint64_t num, int base){
+    bool isSatisfiedWidth = false;
+    int diffblank = 0;
+    int ret = 0;
+
+    // 数字转字符串
+    char str[36];
+    int len = num2string(str, num, base);
+    diffblank += len;
+
+    if (replaceFormat) {
+        if (base == 16) diffblank += 2;
+        else diffblank++;
+    }
+    // 计算需要补几位宽度
+    diffblank = width - diffblank;
+
+    if (paddingCharZero && replaceFormat) {
+        out[ret++] = '0';
+        if (base == 16) {
+            out[ret++] = 'X';
+        }
+    }
+
+    if (diffblank > 0)
+    {   // 如果是右对齐或者使用0补宽度，要在数字前面补
+        if (!leftAlign || paddingCharZero)
+        {
+            for (int j = 0; j < diffblank; j++)
+            {
+                out[ret++] = (paddingCharZero ? '0' : ' ');
+            }
+            isSatisfiedWidth = true;
+        }
+    }
+
+    if (!paddingCharZero && replaceFormat) {
+        out[ret++] = '0';
+        if (base == 16) {
+            out[ret++] = 'x';
+        }
+    }
+
+    strncpy(&out[ret], str, len);
+    ret += len;
+
+    // 如果之前没补宽度，现在补一下
+    if (diffblank > 0 && !isSatisfiedWidth)
+    {
+        for (int j = 0; j < diffblank; j++)
+        {
+            out[ret++] = ' ';
+        }
     }
 
     return ret;
@@ -118,6 +332,8 @@ static void string2num(int *num, const char *string, int len) {
     }
 }
 
+int vsprintf(char *out, const char *fmt, va_list ap);
+
 int printf(const char *fmt, ...) {
     va_list list;
     va_start(list, fmt);
@@ -130,6 +346,7 @@ int printf(const char *fmt, ...) {
     va_end(list);
     return i;
 }
+
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
     int ret = 0;
@@ -147,13 +364,10 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
             paddingCharZero = false;
             width = 0;
             precision = -1;  // TODO
-            length = normal; // TODO
-
-            width = length - 1 + length + 1; // 消除警告
-            width = replaceFormat;
+            length = normal; //
 
             // flags
-            while(1) {
+            while(true) {
                 switch (fmt[i]) {
                     case '-': {
                         leftAlign = true;
@@ -162,7 +376,6 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
                     }
 
                     case '+': {
-                        showSymbol = true;
                         blankBeforePostiveNum = false;
                         i++;
                         break;
@@ -174,7 +387,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
                         break;
                     }
 
-                    case '#': {     // TODO
+                    case '#': {
                         replaceFormat = true;
                         i++;
                         break;
@@ -194,11 +407,10 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
                     default:
                         goto br;
                 }
-br:             break;
             }
 
             // width
-            int j;
+br:         int j;
             for (j = 0; fmt[i + j] <= '9' && fmt[i + j] >= '0'; j++) ;
             string2num(&width, &fmt[i], j);
             i += j;
@@ -214,90 +426,90 @@ br:             break;
             // length
             switch (fmt[i])
             {
-            case 'h': {
-                i++;
-                if (fmt[i] == 'h') {
+                case 'h': {
                     i++;
-                    length = hh;
+                    if (fmt[i] == 'h') {
+                        i++;
+                        length = hh;
+                        break;
+                    }
+                    length = h;
                     break;
                 }
-                length = h;
-                break;
-            }
 
-            case 'l': {
-                i++;
-                if (fmt[i] == 'l') {
+                case 'l': {
                     i++;
-                    length = ll;
+                    if (fmt[i] == 'l') {
+                        i++;
+                        length = ll;
+                        break;
+                    }
+                    length = l;
                     break;
                 }
-                length = l;
-                break;
+
+                // TODO
+                case 'j':
+                case 'z':
+                case 't':
+                case 'L':
+                    length = normal;    // TODO;
+                    i++;
+                default:
+                    break;
             }
 
-            // TODO
-            case 'j':
-            case 'z':
-            case 't':
-            case 'L':
-                length = normal;    // TODO;
-                i++;
-            default:
-                break;
-            }
-
-            uint64_t num;
-            if (fmt[i] != 's' && fmt[i] != 'c' && fmt[i] != '%') {
-                if(length == ll) {
+            uint64_t num = 0;
+            if (fmt[i] != 'c' && fmt[i] != 's' && fmt[i] != 'f') {
+                if (length == ll) {
                     num = va_arg(ap, uint64_t);
                 } else {
                     num = va_arg(ap, uint32_t);
                 }
             }
 
+
             switch (fmt[i]) {
-                case 'i':
                 case 'x':
                 case 'X':
                 case 'o':
+                    ret += handle_x_o(&out[ret], num, fmt[i] == 'o' ? 8 : 16);
+                    break;
                 case 'u':
+                case 'i':
                 case 'd': {
-                    
-                    if (fmt[i] == 'd' || fmt[i] == 'i') {
-                        int64_t _num = num;
-                        if (showSymbol && _num > 0) out[ret++] = '+';
-                        if (blankBeforePostiveNum && _num > 0) out[ret++] = ' ';
-                    }
-                    
-                    int base = 10;
+                    int isSigned = fmt[i] == 'i' || fmt[i] == 'd';
+                    ret += handle_int(&out[ret], num, isSigned);
+                    break;
+                }
 
-                    if (fmt[i] == 'x' || fmt[i] == 'X') base = 16;
-                    if (fmt[i] == '0') base = 8;
-
-                    char temp_str[36];
-                    int len = num2string(temp_str, num, base);
-
-                    len = handle_int(&out[ret], temp_str, len);
-                    ret += len;
-
-
+                case 'f': {
+                    double float_num = va_arg(ap, double);
+                    ret += handle_float(&out[ret], float_num);
                     break;
                 }
 
                 case 's': {
                     char *str = va_arg(ap, char*);
+                    int len = 0;
+                    while(str[len++]);
+                    len--;
+                    if (!leftAlign) for (j = 0; j < width - len; j++) out[ret++] = ' ';
                     while (*str != '\0') {
                         out[ret++] = *(str++);
                     }
+                    if (leftAlign) for (j = 0; j < width - len; j++) out[ret++] = ' ';
                     break;
                 }
 
                 case 'c': {
                     char ch = (char)va_arg(ap, int);
+                    if (!leftAlign) for (j = 0; j < width - 1; j++) out[ret++] = ' ';
                     out[ret++] = ch;
+                    if (leftAlign) for (j = 0; j < width - 1; j++) out[ret++] = ' ';
                     break;
                 }
+
 
                 case '%': {
                     out[ret++] = '%';
