a09fd504d370698d90ab6a0bd3bfe497d0986795
tracer-ysyx <tracer@ysyx.org>
2024-11-13 16:46:17 +0800

    > compile NEMU ysyx_hjw hjw Linux ysyx 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:46:17 up  6:33,  1 user,  load average: 0.13, 0.20, 0.20

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -20,13 +20,12 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
         .width = width_higth >> 16, .height = width_higth & 0x0000ffff,
         .vmemsz = (width_higth >> 16) * (width_higth & 0x0000ffff) * 4
     };
-
-    printf("%d, %d\n", cfg->width, cfg->height);
 }
 
 // AM_DEVREG(11, GPU_FBDRAW,   WR, int x, y; void *pixels; int w, h; bool sync);
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+    printf("111\n");
     if (ctl->h == 0 || ctl->y == 0) return;
     AM_GPU_CONFIG_T cfg;
     __am_gpu_config(&cfg);
