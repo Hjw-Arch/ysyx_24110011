0edc1c70d4c2d806bec2049c4eeb82a99c5083e0
tracer-ysyx <tracer@ysyx.org>
2024-11-13 17:03:37 +0800

    > compile NEMU ysyx_hjw hjw Linux ysyx 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:03:37 up  6:51,  1 user,  load average: 0.53, 0.34, 0.28

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,6 +4,10 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+uint32_t buf[] = {
+    0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 
+    0x00ffff00, 0x00ffff00, 0x00ffff00, 0x00ffff00, 0x00ffff00, 0x00ffff00, 0x00ffff00, 0x00ffff00, 0x00ffff00, 0x00ffff00, 
+};
 void __am_gpu_init() {
     // int i;
     // int w = io_read(AM_GPU_CONFIG).width;
@@ -11,6 +15,8 @@ void __am_gpu_init() {
     // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
     // for (i = 0; i < w * h; i++) fb[i] = i;
     // outl(SYNC_ADDR, 1);
+    io_write(AM_GPU_FBDRAW, 0, 0, buf, 10, 2, 0);
+    io_write(AM_GPU_FBDRAW, 0, 0, 0, 0, 0, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -28,7 +34,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     int width = io_read(AM_GPU_CONFIG).width;
     for (uint32_t i = ctl->y; i < ctl->y + ctl->h; i++) {
         for (uint32_t j = ctl->x; j < ctl->x + ctl->w; j++) {
-            outl(FB_ADDR + (i * width * 4 + j * 4), 114514);
+            outl(FB_ADDR + (i * width * 4 + j * 4), 0x00ffffff);
         }
     }
 
