a0f1524a00c937d402523d596d9b69fea8927049
tracer-ysyx <tracer@ysyx.org>
2024-11-13 22:59:08 +0800

    > compile NEMU ysyx_hjw hjw Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:59:08 up  3:00,  1 user,  load average: 0.05, 0.13, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
@@ -34,5 +35,6 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
     for (uint8_t *i = (uint8_t *)ctl->buf.start; i < (uint8_t *)ctl->buf.end; i++) {
         outb(AUDIO_SBUF_ADDR + io_read(AM_AUDIO_STATUS).count, *i);
         outl(AUDIO_COUNT_ADDR, io_read(AM_AUDIO_STATUS).count + 1);
+        printf("count = %d\n", io_read(AM_AUDIO_STATUS).count);
     }
 }
