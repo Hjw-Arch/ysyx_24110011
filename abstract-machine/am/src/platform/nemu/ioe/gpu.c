21323705c53af0bb37dfa4d5dd3f76bcc63c0b93
tracer-ysyx <tracer@ysyx.org>
2024-11-13 16:12:54 +0800

    > compile NEMU ysyx_hjw hjw Linux ysyx 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:12:54 up  6:00,  1 user,  load average: 0.07, 0.13, 0.16

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,22 +4,46 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+    int i;
+    int w = io_read(AM_GPU_CONFIG).width;
+    int h = io_read(AM_GPU_CONFIG).height;
+    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+    for (i = 0; i < w * h; i++) fb[i] = i;
+    outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+    uint32_t width_higth = inl(VGACTL_ADDR);
+    // *cfg = (AM_GPU_CONFIG_T){
+    //     .present = true, .has_accel = false,
+    //     .width = width_higth >> 16, .height = width_higth & 0x0000ffff,
+    //     .vmemsz = (width_higth >> 16) * (width_higth & 0x0000ffff) * 4
+    // };
+
+    cfg->present = true;
+    cfg->has_accel = false;
+    cfg->width = width_higth >> 16;
+    cfg->height = width_higth & 0x0000ffff;
+    cfg->vmemsz = (width_higth >> 16) * (width_higth & 0x0000ffff) * 4;
 }
 
+// AM_DEVREG(11, GPU_FBDRAW,   WR, int x, y; void *pixels; int w, h; bool sync);
+
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
+    if (ctl->h == 0 || ctl->y == 0) return;
+    AM_GPU_CONFIG_T cfg;
+    __am_gpu_config(&cfg);
+    for (uint32_t i = ctl->y; i < ctl->y + ctl->h; i++) {
+        for (uint32_t j = ctl->x; j < ctl->x + ctl->w; j++) {
+            outl(FB_ADDR + i * cfg.width + j, *((uint32_t *)ctl->pixels + ((i - ctl->y) * cfg.width + (j - ctl->x))));
+        }
+    }
+
+    if (ctl->sync) {
+        outl(SYNC_ADDR, 1);
+    }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
+    status->ready = true;
 }
