de5d9349b6732f880376337650296be109c729d1
tracer-ysyx <tracer@ysyx.org>
2024-11-22 02:55:59 +0800

    > sim RTL ysyx_hjw hjw Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:55:59 up  5:18,  1 user,  load average: 0.15, 0.17, 0.18

--- npc/vsrc/pc.v
+++ npc/vsrc/pc.v
@@ -42,7 +42,7 @@ wire [31 : 0] new_pc = pc_sel == 2'b00 ? adder_res :
                        pc_sel == 2'b11 ? mepc : adder_res;
 
 always @(posedge clk) begin
-    $display("pcsel = %x", adder_res);
+    $display("sum = %x, ina = %x, inb = %x", adder_res, addrA_input, addrB_input);
     if (rst) pc <= RST_VALUE;
     else pc <= new_pc;
 end
