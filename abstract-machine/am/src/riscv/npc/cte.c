546358b65b6afb7f283ed642040c49e8c5f93a70
tracer-ysyx <tracer@ysyx.org>
2024-11-22 10:22:43 +0800

    > sim RTL ysyx_hjw hjw Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:22:43 up  6:55,  1 user,  load average: 0.42, 0.26, 0.23

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -69,6 +69,10 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg)
 
     *(uint32_t *)(kstack.start) = (uint32_t)context;
 
+    printf("\n\nkcontext regs = \n");
+    for (int i = 0; i < NR_REGS; i++) printf("reg%d = 0x%08x\n", i, context->gpr[i]);
+    printf("\n\n");
+
     return context;
 }
 
