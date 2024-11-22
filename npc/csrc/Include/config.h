a443d16ec6e52d66e740946927938abd9fd97b59
tracer-ysyx <tracer@ysyx.org>
2024-11-22 11:12:40 +0800

    > sim RTL ysyx_hjw hjw Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:12:40 up  7:45,  1 user,  load average: 1.20, 0.59, 0.38

--- npc/csrc/Include/config.h
+++ npc/csrc/Include/config.h
@@ -12,7 +12,7 @@
 // #define CONFIG_WATCHPOINT   1
 
 // trace
-#define CONFIG_TRACE        1
+// #define CONFIG_TRACE        1
 // #define CONFIG_MTRACE       1
 // #define CONFIG_FTRACE       1
 // #define CONFIG_ITRACE       1
