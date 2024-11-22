ba22f61f29aa77992eb34457a75552c50ea5c606
tracer-ysyx <tracer@ysyx.org>
2024-11-22 10:17:20 +0800

    > sim RTL ysyx_hjw hjw Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:17:20 up  6:50,  1 user,  load average: 0.11, 0.15, 0.22

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -8,6 +8,15 @@ static Context *(*user_handler)(Event, Context *) = NULL;
 Context *__am_irq_handle(Context *c)
 {
     printf("macuse = %d\n", c->mcause);
+
+    printf("regs = \n");
+    for (int i = 0; i < NR_REGS; i++) printf("reg%d = 0x%08x\n", i, c->gpr[i]);
+
+    printf("mepc = 0x%08x\n", c->mepc);
+
+    printf("context = 0x%08x\n", c);
+
+
     if (user_handler) {
         Event ev = {0};
         switch (c->mcause)
@@ -59,15 +68,6 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg)
     context->mstatus = 0x1800;
 
     *(uint32_t *)(kstack.start) = (uint32_t)context;
-
-    printf("regs = \n");
-    for (int i = 0; i < NR_REGS; i++) printf("reg%d = 0x%08x\n", i, context->gpr[i]);
-
-    printf("mepc = 0x%08x\n", context->mepc);
-
-    printf("context = 0x%08x\n", context);
-
-    printf("stack = 0x%08x\n", kstack.end);
 
     return context;
 }
