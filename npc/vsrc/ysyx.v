10658006ec80d462c22ad06b3639e7ca8c2691cd
tracer-ysyx <tracer@ysyx.org>
2025-03-04 00:19:41 +0800

    > sim RTL ysyx_hjw hjw Linux ysyx 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:19:41 up 3 days, 20:52,  1 user,  load average: 0.14, 0.20, 0.14

--- npc/vsrc/ysyx.v
+++ npc/vsrc/ysyx.v
@@ -55,7 +55,7 @@ wire [WIDTH - 1 : 0] read_data;
 // PC
 
 PC #(WIDTH) PC_INTER(
-    .clk(clk_2_n),
+    .clk(clk_2),
     .rst(rst),
     .sel(pc_sel),
     .sel_for_adder_left(pc_sel_for_adder_left),
@@ -69,7 +69,7 @@ PC #(WIDTH) PC_INTER(
 
 // IF
 IFU #(32) IFU_INTER(
-    .clk(clk_2),
+    .clk(clk_2_n),
     .rst(rst),
     .pc(pc),
     .inst(inst)
@@ -114,7 +114,7 @@ EXU #(32) EXU_INTER(
 
 // MEM
 MEM #(32) MEM_INTER(
-    .clk(clk_2_n),
+    .clk(clk_2),
     .we(mem_we),
     .mem_op(mem_op),
     .write_addr(result),
@@ -131,7 +131,7 @@ wire [WIDTH - 1 : 0] rd_data = rd_input_sel == 2'b01 ? read_data :
 
 
 registerfile #(32) RF_INTER (
-    .clk(clk_2_n),
+    .clk(clk_2),
     .we(rd_we),
     .rd_addr(rd_addr),
     .rd_data(rd_data),
@@ -144,7 +144,7 @@ registerfile #(32) RF_INTER (
 wire [31 : 0] csr_data_in = csr_sel ? rs1_data | csr_data_out : rs1_data;
 
 CSR #(32) CSR_INTER(
-    .clk(clk_2_n),
+    .clk(clk_2),
     .rst(rst),
     .we(csr_we),
     .is_ecall(csr_is_ecall),
